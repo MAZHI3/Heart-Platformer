78373b08d4f0378b36f7e4cec32f2b803f3fc6c6
MAZHI3 <122053601+MAZHI3@users.noreply.github.com>
2024-02-23 13:53:46 -0800

    Progress check 8

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,8 +1,7 @@
 extends Node2D
-@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
-@onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
-@onready var level_completed = $CanvasLayer/LevelCompleted
+@export var next_level: PackedScene
 
+@onready var level_completed = $CanvasLayer/LevelCompleted
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,3 +12,9 @@ func _ready():
 func show_level_completed():
 	level_completed.show()
 	get_tree().paused = true
+	if not next_level is PackedScene: return
+	await LevelTransition.fade_to_black()
+	get_tree().paused = false
+	get_tree().change_scene_to_packed(next_level)
+	LevelTransition.fade_from_black()
+	#get_tree().paused = true
